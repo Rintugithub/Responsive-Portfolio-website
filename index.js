271595971d596c3f400b16a2f24fa3c8a5dd48e6
Rintugithub <rintuva000@gmail.com>
2022-10-23 18:51:15 +0530

    portfolio website

---
+++ index.js
@@ -0,0 +1,38 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function openTab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw1-7eYKTIu2AvUBL1v0Btvp812t4YFjxYR5C1XD2UNhZN3WFxLc7Sy4liBFoPLJVLCUg/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Send Successfully"
+        setTimeout(() => {
+            msg.innerHTML= "";
+        }, 3000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
